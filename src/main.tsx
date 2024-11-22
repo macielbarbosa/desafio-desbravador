7282a49a707bf9e33e14ed6f6bc5842f9bfb1756
Maciel <macielbarbosa@outlook.com>
2024-11-22 15:34:11 -0300

    Resolução de alias

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 
+import { Home } from "@pages/home";
+import { Repositories } from "@pages/repositories";
+import { NotFound } from "@pages/NotFound.tsx";
+
 import "./index.css";
 import App from "./App.tsx";
-import { Home } from "./pages/home";
-import { Repositories } from "./pages/repositories";
-import { NotFound } from "./pages/NotFound.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
