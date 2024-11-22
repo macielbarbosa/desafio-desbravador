34b79a6d2ffff1a1f8f5034f0b72698b62d1b9a1
Maciel <macielbarbosa@outlook.com>
2024-11-22 15:33:54 -0300

    Instalação do axios

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,16 @@
+import { useEffect } from "react";
+
+import { axiosInstance } from "@utils/axiosInstance";
+
 export const Home = () => {
+  const getUser = async () => {
+    const { data } = await axiosInstance("users/macielbarbosa");
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return <p>Home</p>;
 };
