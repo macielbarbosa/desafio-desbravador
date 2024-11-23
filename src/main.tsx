59cbaa2f86b848aba4c97044aa59599d43a81bfd
Maciel <macielbarbosa@outlook.com>
2024-11-23 16:05:57 -0300

    Instalação do Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 import { Home } from "@pages/home";
 import { Repositories } from "@pages/repositories";
