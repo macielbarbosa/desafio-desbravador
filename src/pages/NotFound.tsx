b8964851faa598d9b446c28478fc0ea63000d6ee
Maciel <macielbarbosa@outlook.com>
2024-11-24 05:35:14 -0300

    Melhoria da notfound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,1 +1,27 @@
-export const NotFound = () => <p>Essa página não existe.</p>;
+import { Box, Button } from "@mui/material";
+import { useNavigate } from "react-router";
+
+import KeyboardBackspaceOutlinedIcon from "@mui/icons-material/KeyboardBackspaceOutlined";
+import styled from "styled-components";
+
+export const NotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <Root>
+      <p>Essa página não existe.</p>
+      <Button
+        startIcon={<KeyboardBackspaceOutlinedIcon />}
+        onClick={() => navigate("/")}
+      >
+        Voltar para a busca de usuário
+      </Button>
+    </Root>
+  );
+};
+
+const Root = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: start;
+`;
