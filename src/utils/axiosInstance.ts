7d0618188bee25e3b56a5fa105e8cf86a7c91cc1
Maciel <macielbarbosa@outlook.com>
2024-11-25 06:10:17 -0300

    Adição de personal access token

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,4 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "https://api.github.com/",
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_TOKEN}`,
+  },
 });
